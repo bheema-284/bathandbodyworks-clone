c1f5dee099c2716ba948a514750cc9dfa099da51
avinash405xx <95853893+avinash405xx@users.noreply.github.com>
2021-12-18 16:27:26 +0530

    Add files via upload

--- aromatheropy.js
+++ aromatheropy.js
@@ -1,9 +1,9 @@
 document.querySelector(".menutext1").addEventListener("click" , function () {
-    window.location.href = "fragrance.html";
+    window.location.href = "fregrance.html";
 });
 
 document.querySelector(".menutext2").addEventListener("click" , function () {
-    window.location.href = "moisture.html";
+    window.location.href = "moisturizer.html";
 });
 
 document.querySelector(".menutext3").addEventListener("click" , function () {
@@ -11,20 +11,20 @@ document.querySelector(".menutext3").addEventListener("click" , function () {
 });
 
 document.querySelector(".menutext4").addEventListener("click" , function () {
-    window.location.href = "moisture.html";
+    window.location.href = "moisturizer.html";
 });
 
 document.querySelector(".menutext4").addEventListener("click" , function () {
-    window.location.href = "fragrance.html";
+    window.location.href = "fregrance.html";
 });
 document.querySelector(".menutext5").addEventListener("click" , function () {
-    window.location.href = "fragrance.html";
+    window.location.href = "fregrance.html";
 });
 document.querySelector(".menutext6").addEventListener("click" , function () {
-    window.location.href = "Aromatatheropy.html";
+    window.location.href = "aromatheropy.html";
 });
 document.querySelector(".menutext7").addEventListener("click" , function () {
-    window.location.href = "mens_collection.html";
+    window.location.href = "new&now.html";
 });
 document.querySelector(".menutext8").addEventListener("click" , function () {
     window.location.href = "candles.html";
