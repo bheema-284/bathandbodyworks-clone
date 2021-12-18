de74348e7f7fc0e2bff2d029c0ead9ef67c806fd
avinash405xx <95853893+avinash405xx@users.noreply.github.com>
2021-12-18 17:29:28 +0530

    Add files via upload

---
+++ candles.js
@@ -0,0 +1,20 @@
+
+    document.querySelector(".menutext1").addEventListener("click" , function () {
+        window.location.href = "fragrance.html";
+    });
+  
+    document.querySelector(".menutext2").addEventListener("click" , function () {
+        window.location.href = "moisture.html";
+    });
+  
+    document.querySelector(".menutext3").addEventListener("click" , function () {
+        window.location.href = "bath_and_shower.html";
+    });
+  
+    document.querySelector(".menutext4").addEventListener("click" , function () {
+        window.location.href = "moisture.html";
+    });
+  
+    document.querySelector(".menutext4").addEventListener("click" , function () {
+        window.location.href = "fragrance.html";
+    });
