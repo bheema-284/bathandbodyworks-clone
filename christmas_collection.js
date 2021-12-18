d6b27abc6d9396b9b91641b934a91cee49d9eee8
avinash405xx <95853893+avinash405xx@users.noreply.github.com>
2021-12-18 20:37:38 +0530

    Add files via upload

---
+++ christmas_collection.js
@@ -0,0 +1,31 @@
+document.querySelector(".menutext1").addEventListener("click" , function(){
+    window.location.href="new&now.html";
+})
+
+document.querySelector(".menutext2").addEventListener("click" , function(){
+    window.location.href="christmas_collection.html";
+})
+
+document.querySelector(".menutext3").addEventListener("click" , function(){
+    window.location.href="about.html";
+})
+
+document.querySelector(".menutext4").addEventListener("click" , function(){
+    window.location.href="aromatheropy.html";
+})
+
+document.querySelector(".menutext5").addEventListener("click" , function(){
+    window.location.href="aromatheropy.html";
+})
+
+document.querySelector(".menutext6").addEventListener("click" , function(){
+    window.location.href="mens_collection.html";
+})
+
+document.querySelector(".menutext7").addEventListener("click" , function(){
+    window.location.href="wallflowers.html";
+})
+
+document.querySelector(".menutext8").addEventListener("click" , function(){
+    window.location.href="fregrance.html";
+})
