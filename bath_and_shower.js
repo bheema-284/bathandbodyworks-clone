bea04d35431d252d9ae4e4b035c27e049a94d0a0
avinash405xx <95853893+avinash405xx@users.noreply.github.com>
2021-12-18 16:42:00 +0530

    Add files via upload

---
+++ bath_and_shower.js
@@ -0,0 +1,31 @@
+document.querySelector(".menutext1").addEventListener("click" , function () {
+    window.location.href = "fregrance.html";
+});
+
+document.querySelector(".menutext2").addEventListener("click" , function () {
+    window.location.href = "moisturizer.html";
+});
+
+document.querySelector(".menutext3").addEventListener("click" , function () {
+    window.location.href = "bath_and_shower.html";
+});
+
+document.querySelector(".menutext4").addEventListener("click" , function () {
+    window.location.href = "moisturizer.html";
+});
+
+document.querySelector(".menutext4").addEventListener("click" , function () {
+    window.location.href = "fregrance.html";
+});
+document.querySelector(".menutext5").addEventListener("click" , function () {
+    window.location.href = "fregrance.html";
+});
+document.querySelector(".menutext6").addEventListener("click" , function () {
+    window.location.href = "aromatheropy.html";
+});
+document.querySelector(".menutext7").addEventListener("click" , function () {
+    window.location.href = "new_arival.html";
+});
+document.querySelector(".menutext8").addEventListener("click" , function () {
+    window.location.href = "candles.html";
+});
