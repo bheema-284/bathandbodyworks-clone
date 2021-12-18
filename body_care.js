0623e9b3310a8c267514ff859a0df74fe25fe6d4
avinash405xx <95853893+avinash405xx@users.noreply.github.com>
2021-12-18 18:26:39 +0530

    Add files via upload

--- body_care.js
+++ body_care.js
@@ -1,9 +1,9 @@
 document.querySelector("#frag").addEventListener("click" , function () {
-    window.location.href = "fragrance.html";
+    window.location.href = "fregrance.html";
 });
 
 document.querySelector("#mois").addEventListener("click" , function () {
-    window.location.href = "moisture.html";
+    window.location.href = "moisturizer.html";
 });
 
 document.querySelector("#shower").addEventListener("click" , function () {
@@ -11,11 +11,11 @@ document.querySelector("#shower").addEventListener("click" , function () {
 });
 
 document.querySelector("#aroma").addEventListener("click" , function () {
-    window.location.href = "Aromatatheropy.html";
+    window.location.href = "aromatheropy.html";
 });
 
 document.querySelector(".aromabig").addEventListener("click" , function () {
-    window.location.href = "Aromatatheropy.html";
+    window.location.href = "aromatheropy.html";
 });
 
 document.querySelector("#mens").addEventListener("click" , function () {
@@ -27,11 +27,11 @@ document.querySelector("#newbodycare").addEventListener("click" , function () {
 });
 
 document.querySelector(".menutext1").addEventListener("click" , function () {
-      window.location.href = "fragrance.html";
+      window.location.href = "fregrance.html";
   });
 
   document.querySelector(".menutext2").addEventListener("click" , function () {
-      window.location.href = "moisture.html";
+      window.location.href = "moisturizer.html";
   });
 
   document.querySelector(".menutext3").addEventListener("click" , function () {
@@ -39,22 +39,22 @@ document.querySelector(".menutext1").addEventListener("click" , function () {
   });
 
   document.querySelector(".menutext4").addEventListener("click" , function () {
-      window.location.href = "moisture.html";
+      window.location.href = "moisturizer.html";
   });
 
   document.querySelector(".menutext4").addEventListener("click" , function () {
-      window.location.href = "fragrance.html";
+      window.location.href = "fregrance.html";
   });
   document.querySelector(".menutext5").addEventListener("click" , function () {
-      window.location.href = "fragrance.html";
+      window.location.href = "body_care.html";
   });
   document.querySelector(".menutext6").addEventListener("click" , function () {
-      window.location.href = "Aromatatheropy.html";
+      window.location.href = "aromatheropy.html";
   });
 
   document.querySelector(".menutext7").addEventListener("click" , function () {
       window.location.href = "mens_collection.html";
   });
   document.querySelector(".menutext8").addEventListener("click" , function () {
-      window.location.href = "candles.html";
+      window.location.href = "christmas_collection.html";
   });
