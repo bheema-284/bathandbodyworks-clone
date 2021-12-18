d3a9d47123de33f9aab0b93d6e4aef784b6708f8
avinash405xx <95853893+avinash405xx@users.noreply.github.com>
2021-12-18 17:46:36 +0530

    Add files via upload

---
+++ car_fragrance.js
@@ -0,0 +1,27 @@
+
+
+   document.querySelector(".menutext1").addEventListener("click" , function () {
+    window.location.href = "fregrance.html";
+});
+
+document.querySelector(".menutext2").addEventListener("click" , function () {
+    window.location.href = "home_fragrances.html";
+});
+
+document.querySelector(".menutext3").addEventListener("click" , function () {
+    window.location.href = "candles.html";
+});
+
+document.querySelector(".menutext4").addEventListener("click" , function () {
+    window.location.href = "moisturizer.html";
+});
+
+document.querySelector(".menutext4").addEventListener("click" , function () {
+    window.location.href = "bady_care.html";
+});
+
+
+document.querySelector(".menutext5").addEventListener("click" , function () {
+    window.location.href = "christmas.html";
+});
+
